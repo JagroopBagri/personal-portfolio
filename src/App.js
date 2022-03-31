480e0453b206696cde9266e6e9465d25f2cb6f1f
Jagroop <jagroopbagri5@gmail.com>
2022-03-30 17:22:11 -0700

    Made project component reusuable via props and added first project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import Header from './components/header';
 import Intro from './components/intro';
 import Project from './components/project';
+import weatherApp from './images/weather-app.png';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Intro />
-      <Project />
+      <Project
+        title="Weather App"
+        image={weatherApp}
+        link="https://jagroopbagri.github.io/Weather-App/"
+        repo="https://github.com/JagroopBagri/Weather-App"
+        summary="A weather app that uses OpenWeather API to retrieve
+        the current weather from any city or country in the world. Retrieved JSON data using Fetch API along with async/await. "
+        technology="JavaScript, HTML, CSS, Webpack"
+      />
     </div>
   );
 }
--- src/components/project.js
+++ src/components/project.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import weatherApp from '../images/weather-app.png';
 
 function Project(props) {
   return (
     <div className="project">
-      <div className="project--title">Weather App</div>
+      <div className="project--title">{props.title}</div>
       <div className="project--info">
-        <img className="project--pic" src={weatherApp} alt="weather-app" />
+        <img className="project--pic" src={props.image} alt="project-pic" />
         <div className="project--details">
           <div className="project--links">
-            <p>Live Link</p>
-            <p>Repo</p>
+            <a href={props.link} target="_blank" rel="noreferrer">
+              Live Link
+            </a>
+            <a href={props.repo} target="_blank" rel="noreferrer">
+              Repo
+            </a>
           </div>
+          <div className="project-summary">{props.summary}</div>
+          <p className="project--technology">{props.technology}</p>
         </div>
-        <div className="project-summary">
-          This project is a weather app that uses openweather api to retrieve
-          the weather from any city or country in the world.
-        </div>
-        <div className="project-technology">JavaScript, HTML, CSS, Jest</div>
       </div>
     </div>
   );
